4ea5e0912c3348b330caae01db87411779d72bed
Frank G <frank@intrahouse.com>
2018-08-09 19:28:52 +0200

    Added electron for building desktop app

---
+++ index.js
@@ -0,0 +1,30 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({width: 800, height: 600})
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'electron.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+    // Open devtools
+    win.webContents.openDevTools();
+
+    win.on('closed', () => {
+        win = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin'){
+        app.quit();
+    }
+});
