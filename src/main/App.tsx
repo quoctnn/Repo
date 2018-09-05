512f0324a0efcf9dd0be699418ed9e2b5d9f253c
Frank G <frank@intrahouse.com>
2018-09-05 17:49:58 +0200

    Serviceworker update script

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -18,104 +18,114 @@ import { RootReducer } from '../reducers/index';
 import { UserProfile } from '../reducers/profileStore';
 import { PaginatorAction } from '../reducers/createPaginator';
 import ApiClient from '../network/ApiClient';
-import ChannelEventStream from "../components/general/ChannelEventStream"; 
-import * as Actions from "../actions/Actions" 
+import ChannelEventStream from '../components/general/ChannelEventStream';
+import * as Actions from '../actions/Actions';
 require('jquery/dist/jquery');
 require('popper.js/dist/umd/popper');
 require('bootstrap/dist/js/bootstrap');
 require('../utilities/Extensions');
 
-OfflinePluginRuntime.install();
+OfflinePluginRuntime.install({
+  onUpdateReady: () => {
+    OfflinePluginRuntime.applyUpdate();
+  },
+  onUpdated: () => {
+    resetCachedDataAndReload();
+  }
+});
 
-
-const loggingMiddleware = store => next => action => { 
-    console.log("DISPATCHING => ", action)
-    let result = next(action)
-    console.log('NEXT STATE => ', store.getState())
-    return result
-}
-const applyTheme = (themeIndex:number) => {
-    let theme = availableThemes[themeIndex]
-    let selector = theme.selector
-    let root = document.querySelector(":root")
-    root.className = selector
-}
+const loggingMiddleware = store => next => action => {
+  console.log('DISPATCHING => ', action);
+  let result = next(action);
+  console.log('NEXT STATE => ', store.getState());
+  return result;
+};
+const applyTheme = (themeIndex: number) => {
+  let theme = availableThemes[themeIndex];
+  let selector = theme.selector;
+  let root = document.querySelector(':root');
+  root.className = selector;
+};
 const themeSwitcherMiddleware = store => next => action => {
-    let result = next(action)
-    if (action.type === Types.SET_THEME) 
-    {
-        let state = store.getState() as RootReducer
-        applyTheme(state.settings.theme)
-    }
-    return result
-}
+  let result = next(action);
+  if (action.type === Types.SET_THEME) {
+    let state = store.getState() as RootReducer;
+    applyTheme(state.settings.theme);
+  }
+  return result;
+};
 const paginationMiddleware = store => next => action => {
-    let result = next(action)
-    if (action.type === Types.REQUEST_PAGE) 
-    {
-        let a = action as PaginatorAction
-        ApiClient.getPage(a.meta.endpoint,a.payload.page , a.meta.pageSize, (data, status, error) => {
-
-            let receivePageAction = (  page:number, results:any[], total:number):PaginatorAction => ({
-                type:  Types.RECEIVE_PAGE,
-                payload: {
-                    page,
-                    results,
-                    total:total
-                },
-                meta: a.meta
-            })
-            store.dispatch(receivePageAction(a.payload.page, data.results || [], data.count || 0))
-        })
-    }
-    return result
-}
-var middleWares = [loggingMiddleware, paginationMiddleware]
-if(Settings.supportsTheming)
-{
-    middleWares.push(themeSwitcherMiddleware)
+  let result = next(action);
+  if (action.type === Types.REQUEST_PAGE) {
+    let a = action as PaginatorAction;
+    ApiClient.getPage(
+      a.meta.endpoint,
+      a.payload.page,
+      a.meta.pageSize,
+      (data, status, error) => {
+        let receivePageAction = (
+          page: number,
+          results: any[],
+          total: number
+        ): PaginatorAction => ({
+          type: Types.RECEIVE_PAGE,
+          payload: {
+            page,
+            results,
+            total: total
+          },
+          meta: a.meta
+        });
+        store.dispatch(
+          receivePageAction(a.payload.page, data.results || [], data.count || 0)
+        );
+      }
+    );
+  }
+  return result;
+};
+var middleWares = [loggingMiddleware, paginationMiddleware];
+if (Settings.supportsTheming) {
+  middleWares.push(themeSwitcherMiddleware);
 }
 const store = createStore(appReducer, applyMiddleware(...middleWares));
 
-export const getProfileById = (id:number):UserProfile => 
-{
-    let s = store.getState()
-    if(s.profile.id == id)
-        return s.profile
-    return  s.profileStore.byId[id]
-}
-const resetCachedDataAndReload = () => 
-{
-    if (confirm('A new version of intraWork is available. Do you want to update now?')) 
-    {
-        store.dispatch(Actions.resetPagedData())
-        window.location.reload() 
-    }
-}
-const persistor = persistStore(store, { }, () => 
-{ 
-    //rehydrate complete
-    if(Settings.supportsTheming)
-    {
-        let themeIndex = store.getState().settings.theme || 0
-        applyTheme(themeIndex)
-    }
-    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) 
-    { 
-        resetCachedDataAndReload()
-    } 
-})
+export const getProfileById = (id: number): UserProfile => {
+  let s = store.getState();
+  if (s.profile.id == id) return s.profile;
+  return s.profileStore.byId[id];
+};
+const resetCachedDataAndReload = () => {
+  if (
+    confirm(
+      'A new version of intraWork is available. Do you want to update now?'
+    )
+  ) {
+    store.dispatch(Actions.resetPagedData());
+    window.location.reload();
+  }
+};
+const persistor = persistStore(store, {}, () => {
+  //rehydrate complete
+  if (Settings.supportsTheming) {
+    let themeIndex = store.getState().settings.theme || 0;
+    applyTheme(themeIndex);
+  }
+  if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+    resetCachedDataAndReload();
+  }
+});
 ReactDOM.render(
-        <Provider store={store}>
-            <>
-                <ChannelEventStream />
-                <PersistGate loading={null} persistor={persistor}>
-                    <AutoIntlProvider>
-                        <Main/>
-                    </AutoIntlProvider>
-                </PersistGate>
-            </>
-        </Provider>,
-    document.getElementById("app-root")
+  <Provider store={store}>
+    <>
+      <ChannelEventStream />
+      <PersistGate loading={null} persistor={persistor}>
+        <AutoIntlProvider>
+          <Main />
+        </AutoIntlProvider>
+      </PersistGate>
+    </>
+  </Provider>,
+  document.getElementById('app-root')
 );
-export default store 
+export default store;
--- src/reducers/settings.ts
+++ src/reducers/settings.ts
@@ -1,31 +1,30 @@
-import {Types} from "../utilities/Types"
-export interface StyleTheme
-{
-    name:string
-    selector:string
+import { Types } from '../utilities/Types';
+export interface StyleTheme {
+  name: string;
+  selector: string;
 }
-export const availableThemes:StyleTheme[] = [
-            { name: "Default", selector:""}, 
-            { name: "Light", selector:"light"}, 
-            { name: "Dark", selector:"dark"}]
+export const availableThemes: StyleTheme[] = [
+  { name: 'Default', selector: '' },
+  { name: 'Light', selector: 'light' },
+  { name: 'Dark', selector: 'dark' }
+];
 
-export const availableLanguages = ["en", "es", "no"]
-const INITIAL_STATE = { 
-            language: 0,
-            awayTimeout:300,
-            theme:0
-        }
+export const availableLanguages = ['en', 'es', 'no'];
+const INITIAL_STATE = {
+  language: 0,
+  awayTimeout: 300,
+  theme: 0
+};
 const settings = (state = INITIAL_STATE, action) => {
-    switch(action.type) 
-    {
-        case Types.SET_LANGUAGE:
-            return { ...state, language: action.language}
-        case Types.SET_THEME:
-            return { ...state, theme: action.theme}
-        case Types.SET_AWAY_TIMEOUT:
-            return { ...state, awayTimeout: action.timeout}
-        default:
-            return state;
-    }
-}
-export default settings
+  switch (action.type) {
+    case Types.SET_LANGUAGE:
+      return { ...state, language: action.language };
+    case Types.SET_THEME:
+      return { ...state, theme: action.theme };
+    case Types.SET_AWAY_TIMEOUT:
+      return { ...state, awayTimeout: action.timeout };
+    default:
+      return state;
+  }
+};
+export default settings;
