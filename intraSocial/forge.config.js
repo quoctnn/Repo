6690c4aa7381b2442953634947933e333af618c8
Frank G <frank@intrahouse.com>
2019-08-13 15:33:25 +0200

    Electron

--- intraSocial/forge.config.js
+++ intraSocial/forge.config.js
@@ -2,9 +2,15 @@ path = require('path');
 
 module.exports = {
     packagerConfig: {
-        icon: path.resolve(__dirname, 'app/assets/icons/png/512x512.png')
+        icon: path.resolve(__dirname, 'assets/icons/png/512x512.png')
     },
     makers: [
+        {
+            name: '@electron-forge/maker-dmg',
+            config: {
+                format: 'ULFO'
+            }
+        },
         {
             name: '@electron-forge/maker-zip',
             platforms: [
@@ -17,7 +23,7 @@ module.exports = {
                 options: {
                     maintainer: 'IntraHouse AS',
                     homepage: 'https://intrahouse.com',
-                    icon: path.resolve(__dirname, 'app/assets/icons/png/512x512.png')
+                    icon: path.resolve(__dirname, 'assets/icons/png/512x512.png')
                 }
             }
         },
