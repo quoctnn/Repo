4ce416955d92153ebe4cf02dd0efe87708ba87a0
Frank G <frank@intrahouse.com>
2019-09-30 13:46:46 +0200

    Added websocket disconnect popup

--- src/app/components/error/GlobalErrorBoundary.tsx
+++ src/app/components/error/GlobalErrorBoundary.tsx
@@ -1,33 +1,51 @@
 import * as React from "react";
 import SimpleDialog from "../general/dialogs/SimpleDialog";
 import {ApiClient} from "../../network/ApiClient";
-import { CrashLogLevel } from "../../types/intrasocial_types";
+import { CrashLogLevel, NotificationObject } from '../../types/intrasocial_types';
 import { AuthenticationManager } from "../../managers/AuthenticationManager";
 import { EndpointManager } from '../../managers/EndpointManager';
+import { EventSubscription } from 'fbemitter';
+import ReconnectingWebSocket from 'reconnecting-websocket';
+import { NotificationCenter } from "../../utilities/NotificationCenter";
+import ChannelEventStream, { eventStreamNotificationPrefix, EventStreamMessageType } from "../../network/ChannelEventStream";
+import "./GlobalErrorBoundary.scss"
+import { applyMiddleware } from 'redux';
+import classnames from 'classnames';
+import { translate } from '../../localization/AutoIntlProvider';
 type State = {
     error:Error
     errorInfo:any
     windowError:Error
+    websocket:number
 }
 type Props = {}
 export default class GlobalErrorBoundary extends React.Component<Props, State> {
-    constructor(props:Props) {
+    observers:EventSubscription[] = []
+        constructor(props:Props) {
         super(props);
-        this.state = { error: null,  errorInfo:null, windowError:null};
+        this.state = { error: null,  errorInfo:null, windowError:null, websocket:null};
+        const stateChangeObserver = NotificationCenter.addObserver(eventStreamNotificationPrefix + EventStreamMessageType.SOCKET_STATE_CHANGE, this.websocketStateChangedEvent)
+        this.observers.push(stateChangeObserver)
     }
     componentWillMount = () => {
         window.addEventListener('error', this.onWindowError)
     }
+    componentWillUnmount() {
+        this.observers.forEach(o => o.remove())
+    }
     onWindowError = (event: ErrorEvent) => {
         this.setState({ windowError:event.error })
         this.sendError(event.error)
+    }
+    websocketStateChangedEvent = (...args:any[]) => {
+        this.setState({websocket: args[0]})
     }
     componentDidCatch(error:Error, errorInfo:any) {
         this.setState({ error, errorInfo })
         this.sendError(error, errorInfo)
     }
     closeWindowErrorModal = () => {
-        this.setState((prevState:State) => { 
+        this.setState((prevState:State) => {
             return {windowError:null}
          })
     }
@@ -52,6 +70,21 @@ export default class GlobalErrorBoundary extends React.Component<Props, State> {
 
         })
     }
+    ignoreWebSocketError = (e:React.MouseEvent) => {
+        this.setState({websocket:0})
+    }
+    renderWebsocketError = () => {
+        const classes = classnames("websocket-error",
+            {active: this.state.websocket == ReconnectingWebSocket.CLOSED})
+        return (
+            <div className={classes}>
+                <div className="text-center">
+                    <h5>{translate("websocket.error.title")}</h5>
+                    <h6>{translate("websocket.error.description")}</h6>
+                    <button className="btn" onClick={this.ignoreWebSocketError}>{translate("common.close")}</button>
+                </div>
+            </div>)
+    }
     renderErrorPage = (error:Error, errorInfo?:any) => {
         if(!error)
             return null
@@ -71,12 +104,13 @@ export default class GlobalErrorBoundary extends React.Component<Props, State> {
                 </div>
     }
     render() {
-        if (this.state.error && this.state.errorInfo) 
+        if (this.state.error && this.state.errorInfo)
         {
                 return this.renderErrorPage(this.state.error, this.state.errorInfo)
         }
         return <>
             {this.renderWindowErrorModal()}
+            {this.renderWebsocketError()}
             {this.props.children}
             </>
     }
