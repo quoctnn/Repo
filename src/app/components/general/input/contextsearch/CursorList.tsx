9cbfb319a7980d87e67497c9e200b01beaf1468b
quoctnn <mondayb916@gmail.com>
2019-12-19 15:15:13 +0100

    created paging on search

--- src/app/components/general/input/contextsearch/CursorList.tsx
+++ src/app/components/general/input/contextsearch/CursorList.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import classNames from "classnames";
 import { nullOrUndefined } from '../../../../utilities/Utilities';
+import Pagination from "react-pagination-library";
+import "./Pagination.css";
 
 export class CursorListItem extends React.Component<{children?:React.ReactNode, onSelect:() => void}, {}>
 {
     render(){
-        return <div className="item">{this.props.children}</div>
+        return <div className="item">{this.props.children} </div>
     }
 }
 export class CursorListHeader extends React.Component<{children?:React.ReactNode}, {}>
@@ -24,8 +26,11 @@ type OwnProps = {
 }
 type DefaultProps = {
 }
+
 type State = {
     cursor:number
+    currentPage: number,
+    todosPerPage: number
 }
 type Props = OwnProps & DefaultProps
 export default class CursorList extends React.Component<Props, State> {
@@ -35,9 +40,10 @@ export default class CursorList extends React.Component<Props, State> {
     }
     constructor(props:Props) {
         super(props);
-
         this.state = {
             cursor:this.getInitialCursor(props),
+            currentPage: 1,
+            todosPerPage: 5           
         }
     }
     getInitialCursor = (props) => {
@@ -50,6 +56,7 @@ export default class CursorList extends React.Component<Props, State> {
         document.removeEventListener("keydown", this.onKeyDown)
         this.component = null
     }
+
     handleItemClick = (item:JSX.Element, event:any) => {
         if(!item || item.type != cursorListItemComparer.type)
             return
@@ -63,7 +70,8 @@ export default class CursorList extends React.Component<Props, State> {
             return
         }
         this.setState({cursor:-1})
-    }
+    } 
+
     getSuggestionsCount = () => {
         return this.props.items.filter(n => {
             return n.type === cursorListItemComparer.type
@@ -155,27 +163,49 @@ export default class CursorList extends React.Component<Props, State> {
     onItemSelect = (item:JSX.Element) => (event:any) => {
         this.handleItemClick(item, event)
     }
+    changeCurrentPage = (numPage) => {
+        this.setState({ cursor: numPage, currentPage: numPage })
+    }
+
     render = () => {
+        const pageNumbers = [];
+        for (let i = 1; i <= Math.ceil( this.props.items.length / this.state.todosPerPage); i++) {
+          pageNumbers.push(i);
+        }
         const cursor = this.state.cursor
         return (<div ref={this.component}
-                    className="autocomplete-component"
+                    className="autocomplete-component"                                                           
                     style={this.props.style}
-                    onTouchStart={this.onListMouseDown}
+                    onTouchStart={this.onListMouseDown}                                                                                                                                                                                                                                                                                              
                     onMouseDown={this.onListMouseDown}
                     onMouseLeave={this.onComponentMouseLeave}
                     >
-                    {<div className="list" ref={this.listRef}>
-                        {this.props.items.map((item, index) => {
+                   <Pagination
+                    currentPage={pageNumbers[this.state.cursor]}
+                    showNext = {true}
+                    showPrevious = {true}
+                    showLast 
+                    totalPages={pageNumbers.length}
+                    showFirst
+                    changeCurrentPage={this.changeCurrentPage}
+                    theme="square-fill"    
+                    />         
+                    {          
+                        <div className="list" ref={this.listRef}>
+                        {    
+                        this.props.items.map((item, index) => {
                             if(item.type === cursorListItemComparer.type)
                             {
                                 const cn = classNames("cursor-list-item", {"active":cursor == index})
-                                return <div onMouseEnter={this.onMouseEnter.bind(this, index)} className={cn} key={index} onClick={this.onItemSelect(item)}>
-                                            {item}
-                                        </div>
+                                const indexOfLastTodo = this.state.currentPage * this.state.todosPerPage;
+                                const indexOfFirstTodo = indexOfLastTodo - this.state.todosPerPage;
+                                const currentTodos = this.props.items.slice(indexOfFirstTodo, indexOfLastTodo);                               
+                                return <div onMouseEnter={this.onMouseEnter.bind(this, index)} className={cn} key={index} onClick={this.onItemSelect(currentTodos[index])}>
+                                {currentTodos[index]}  
+                            </div>
                             }
-                            return item
                         })}
-                    </div>}
+                     </div>}
                 </div>)
     }
 }
